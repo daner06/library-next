b8265f164d7c3ff5e09afbf689b5ecb7d1a3a39a
Rescps <daner06@gmail.com>
2024-07-05 16:37:14 +0100

    improve page design and tabs

--- src/app/page.styles.tsx
+++ src/app/page.styles.tsx
@@ -1,19 +1,58 @@
 import styled from "@emotion/styled";
 
+// should be moved into the design template.
+const backgroundPageColor = "#f8f9fc";
+const backgroundSecondaryColor = "#f1f5f8";
+const backgroundTertiaryColor = "#fff";
+const fontPrimaryColor = "#000";
+const borderPrimary = "#e2e7f1";
+
 export const StyledContainer = styled("div")`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   width: 50rem;
+  backgroud: ${backgroundPageColor};
 `;
 
 export const StyledH1 = styled("h1")`
   padding-bottom: 1.5rem;
 `;
 
+export const StyledDescription = styled("h3")`
+  font-weight: 400;
+`;
+
 export const StyledSearchBar = styled("input")`
   width: 100%;
   padding: 0.5rem;
   margin-top: 1.5rem;
+  border: 1px solid ${borderPrimary};
+  border-radius: 0.25rem;
+  color: ${fontPrimaryColor};
+  background: ${backgroundTertiaryColor};
+`;
+
+export const StyledTabs = styled("div")`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1.5rem;
+  background: ${backgroundSecondaryColor};
+  width: 100%;
+  height: 2.75rem;
+  border-radius: 0.25rem;
+`;
+
+export const StyledTabButton = styled("button", {
+  shouldForwardProp: (prop) => prop !== "active",
+})<{ active?: Boolean }>`
+  background: ${({ active }) =>
+    active ? backgroundTertiaryColor : backgroundSecondaryColor};
+  color: ${fontPrimaryColor};
+  width: 100%;
+  border: none;
+  margin: 0.375rem;
+  border-radius: 0.25rem;
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,72 @@
 "use client";
 
 import styles from "./page.module.css";
-import { StyledContainer, StyledH1, StyledSearchBar } from "./page.styles";
+import {
+  StyledContainer,
+  StyledDescription,
+  StyledH1,
+  StyledSearchBar,
+  StyledTabButton,
+  StyledTabs,
+} from "./page.styles";
+import { useState } from "react";
+
+type Tabs = {
+  key: string;
+  text: string;
+};
+
+const tabs: Tabs[] = [
+  {
+    key: "featured",
+    text: "Featured",
+  },
+  {
+    key: "kpi",
+    text: "KPI",
+  },
+  {
+    key: "layouts",
+    text: "Layouts",
+  },
+  {
+    key: "storyboards",
+    text: "Storyboards",
+  },
+];
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("featured");
+
+  const handleTabClick = (key: string) => setActiveTab(key);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <StyledContainer>
           <StyledH1>Library</StyledH1>
-          <p>Browse for assets needed to report and present analysis.</p>
+          <StyledDescription>
+            Browse for assets needed to report and present analysis.
+          </StyledDescription>
           <StyledSearchBar
             id="search"
             type="search"
             placeholder="Type to search..."
           />
+          <StyledTabs>
+            {tabs.map((tab) => {
+              const { key, text } = tab;
+              return (
+                <StyledTabButton
+                  key={key}
+                  active={activeTab === key}
+                  onClick={() => handleTabClick(key)}
+                >
+                  {text}
+                </StyledTabButton>
+              );
+            })}
+          </StyledTabs>
         </StyledContainer>
       </div>
     </main>
